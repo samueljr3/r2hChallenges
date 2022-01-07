07de2774c0055a73dba97a5dac366dfaed29cd8c
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-01-07 10:12:16 -0500

    easy commit

---
+++ jsWeek1/Easy/easy.js
@@ -0,0 +1,9 @@
+let name1 = "Reggie";
+let name2 = "Dyl";
+
+if(name1.length > name2.length){
+    console.log(`The name ${name1} is longer than ${name2} by ${name1.length - name2.length} characters`);
+}
+else{
+    console.log(`The name ${name2} is longer than ${name1} by ${name2.length - name1.length} characters`);
+}
