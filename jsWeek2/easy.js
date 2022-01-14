5aca3a7f6ab82f9edd9ae8d28b36671fbe57a69e
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-01-13 20:31:57 -0500

    first commit for jsWeek2

---
+++ jsWeek2/easy.js
@@ -0,0 +1,3 @@
+let students = ["Rivas", "Sam", "20", "Doe", "John", "19", "Barack", "Obama", "60"];
+
+console.log(`Hello, my name is ${students[4]} ${students[3]} and I'm ${students[5]} years old.`);
---
+++ jsWeek2/veryEasy/script.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2  = 44;
+
+function min(x, y){
+    if(x > y){
+        return y;
+    }
+    else{
+        return x;
+    }
+}
+
+console.log(min(num1, num2));
