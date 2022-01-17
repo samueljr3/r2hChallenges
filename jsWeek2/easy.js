46295ddbc26f66d7e7036c18721f4bc4c27ad49e
Samuel Rivas Ramirez <samuelrivasramirez@r2hs-mbp.lan>
2022-01-16 23:49:54 -0500

    easy fix

---
+++ jsWeek2/easy.js
@@ -0,0 +1,9 @@
+let students = {
+    name:["Samuel Rivas Ramirez", "John Doe", "Barack Obama"],
+    age:[20, 19, 60]
+};
+
+
+console.log(`Hello, my name is ${students.name[1]} and I'm ${students.age[1]} years old.`);
+
+
